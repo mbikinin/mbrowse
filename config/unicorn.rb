a1b112329ae1bbfdf8976de8b39b5c2a009c18a4
maksim844 <cot844@yandex.ru>
2013-10-24 00:40:02 +0400

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-deploy_to  = "/var/www/mbrowse"
+deploy_to  = "/var/www/mbrowse/"
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
@@ -7,10 +7,9 @@ log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
 
-
-timeout 60
-worker_processes 4 # Здесь тоже в зависимости от нагрузки, погодных условий и текущей фазы луны
-listen socket_file, :backlog => 2048
+timeout 30
+worker_processes 16 # Здесь тоже в зависимости от нагрузки, погодных условий и текущей фазы луны
+listen socket_file, :backlog => 1024
 pid pid_file
 stderr_path err_log
 stdout_path log_file
