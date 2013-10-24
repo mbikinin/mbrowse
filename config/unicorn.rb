14531f316981ea1d2526fc8b909816ec026f2d1a
maksim844 <cot844@yandex.ru>
2013-10-25 00:18:16 +0400

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-deploy_to  = "/var/www/mbrowse/"
+deploy_to  = "/var/www/mbrowse"
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
